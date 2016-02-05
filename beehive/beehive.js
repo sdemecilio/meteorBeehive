99d64f8b9d9747e4c5730645514ab0078439ae67
sdemecilio <stacey.demecilio@live.com>
2016-02-05 01:19:13 -0800

    Basic look of form, no functions working yet.

--- beehive/beehive.js
+++ beehive/beehive.js
@@ -1,23 +1,42 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Hivename = new Mongo.Collection('hiveName');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+if (Meteor.isClient)
+{
+	Meteor.subscribe("hiveName");
+	
+	Template.collectionForm.events({});
+	
+	Template.collectionForm.events
+	({
+		'submit form': function(event)
+		{
+			event.preventDefault();
+			var hive = $(event.target).find('textarea[name=hiveName]');
+			var hiveText = hive.val();
+			var observation = $(event.target).find('input[name=observationDate]');
+			var observationValue = observation.val();
+			var durationTime = $(event.target).find('input[name=duration]');
+			var duration = durationTime.val();
+			var mite = $(event.target).find('input[name=miteCount]');
+			var miteValue = mite.val();
+			
+			Hivename.insert({hiveName: hiveText, observationDate: observationValue, duration: duration, miteCount: miteValue, sumbitedOn: Date.now()});
+		}
+	});
+	
+	Template.collectionForm.helpers
+	({
+		'Hivename':function()
+		{
+			return Hivename.find({}, {sort: {submittedOn: -1}}) || {};
+		}
+	})
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if (Meteor.isServer)
+{
+	// code only runs on server side 
+	Meteor.publish("hiveName", function(){
+		return Hivename.find();
+	})
 }
